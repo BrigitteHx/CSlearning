db5a6d4786afb7b35532ca2b7a13a40a52df5f55
BrigitteHx <brigitteheijkoop@gmail.com>
2025-02-11 13:13:05 +0100

    CS learning cursus 1, S1 t/m S3 added

---
+++ CSlearningMap1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check the notes file for the code I tested in this course :)");
+        }
+    }
+}
