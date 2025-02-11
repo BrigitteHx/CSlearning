7595d2f951e8b32bb3b395527392cb6731809541
BrigitteHx <brigitteheijkoop@gmail.com>
2025-02-11 17:47:22 +0100

    CS learning cursus, S5 added

---
+++ CSlearningMap1/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+            for (var i = 0 < passwordLength; i++)
+            {
+                var randomIndex = random.Next(0, 3);
+                buffer[i] = (char)('a' + randomIndex);
+            }
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
