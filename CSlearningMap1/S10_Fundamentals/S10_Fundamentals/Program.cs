e557756b61bb2ef9b87cdd75a9e488556af72e70
BrigitteHx <brigitteheijkoop@gmail.com>
2025-02-11 20:46:35 +0100

    CS learning cursus, S10 added, basics cursus voltooid

---
+++ CSlearningMap1/S10_Fundamentals/S10_Fundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace S10_Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // F9 = breakpoint
+            // F5 = start debugging (shift+ F5 = stop debugging)
+            // crtl + F5 = start without debugging
+            // F10 = step over
+            // F11 = step into
+            // shift + F11 = step out
+
+            // defensive programming: always assume that the user will enter invalid data so check ANY value that comes from the user before moving on to other code.
+
+            // windows:
+            // call stack: shows the order of methods that have been called
+            // locals: shows the values of variables in the current method/ scope.
+            // autos: shows the values of variables that are used in the current line of code.
+            // watch: allows you to watch the value of a variable or expression.
+
+
+        }
+    }
+}
