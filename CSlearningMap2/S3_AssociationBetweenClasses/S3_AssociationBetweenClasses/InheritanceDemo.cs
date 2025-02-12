3b88d1262edbb965eb6c39ffc4374191f9b3cf2e
BrigitteHx <brigitteheijkoop@gmail.com>
2025-02-12 14:17:08 +0100

    CS learning cursus 2, S3 added

---
+++ CSlearningMap2/S3_AssociationBetweenClasses/S3_AssociationBetweenClasses/InheritanceDemo.cs
@@ -0,0 +1,42 @@
+﻿//using static System.Net.Mime.MediaTypeNames;
+
+//namespace S3_AssociationBetweenClasses
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var text = new Text();
+//            text.Width = 100;
+//            text.Copy();
+//        }
+//    }
+
+//    public class PresentationObject
+//    {
+//        public int Width { get; set; }
+//        public int Height { get; set; }
+
+//        public void Copy()
+//        {
+//            Console.WriteLine("Object copied to clipboard.");
+//        }
+
+//        public void Duplicate()
+//        {
+//            Console.WriteLine("Object was duplicated.");
+//        }
+//    }
+
+//    public class Text : PresentationObject
+//    {
+//        public int FontSize { get; set; }
+//        public string FontName { get; set; }
+
+//        public void AddHyperlink(string url)
+//        {
+//            Console.WriteLine("We added a link to " + url);
+//        }
+
+//    }
+//}
---
+++ CSlearningMap2/S3_AssociationBetweenClasses/S3_AssociationBetweenClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace S3_AssociationBetweenClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
