3eee073e8622bfd4386adc93a7478531d638362b
BrigitteHx <brigitteheijkoop@gmail.com>
2025-02-13 14:20:16 +0100

    CS learning cursus 2, S6 added

---
+++ CSlearningMap2/S6_Interfaces/Extensibility/Extensibility/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\Users\\brigi\\Documents\\AA - Stage2\\CSlearning\\CSlearningMap2\\S6_Interfaces\\Extensibility\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
